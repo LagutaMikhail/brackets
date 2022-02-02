48806714ca1105a8006da2503e2d5da5d914a322
Mikhail Laguta <lagutamihail@gmail.com>
2022-02-02 21:03:56 +0300

    feat: add some code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let openBrackets =[];
+  for (let i=0; i<bracketsConfig.length; i++){
+    openBrackets.push(bracketsConfig[i][0])
+  };
+  let closeBrackets =[];
+  for (let i=0; i<bracketsConfig.length; i++){
+    closeBrackets.push(bracketsConfig[i][1])
+  };
+
+  let s = [];
+  for (let i = 0; i<str.length; i++){
+    if (s.length==0&&closeBrackets.includes(str[i])){
+      return false;
+      break;
+    }
+    else if (openBrackets.includes(str[i])){
+      s.push(str[i]);
+    }
+    else if (closeBrackets.includes(str[i])&&s.length!==0){
+      bracketsConfig.forEach(function (item,index) {
+        if (item[1] == str[i]&&item[0]==s[s.length-1]){
+          s.pop();
+        };
+      });
+      }
+    else {
+      return false;
+      break;
+    }
+    }
+    return s.length==0;
 }
